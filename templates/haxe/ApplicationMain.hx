10dde85498a982b58d320a828844ba0a61da1a1a
Cory Martin <coryamartin@me.com>
2024-01-15 16:46:19 -0600

    Don't read gamepad mappings too early because it changes save directory. Lime's applicationStorageDirectory "variable" calls a getter which seems to prevent Stencyl from setting the save game directory. Also it's not going to use the correct directory at this point anyway.

--- templates/haxe/ApplicationMain.hx
+++ templates/haxe/ApplicationMain.hx
@@ -61,12 +61,6 @@ using StringTools;
 			Sys.setCwd(StencylCppia.gamePath);
 		#end
 
-		#if extra_gamepad_mappings
-		// Add mappings early so that they may apply to already plugged in gamepads.
-		// The ones added in lime.ui.Window are added too late, so we include those too.
-		scripts.GamepadDB.addExtraGamepadMappings();
-		#end
-		
 		Config.load();
 		Input.loadInputConfig();
 		
